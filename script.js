1f650b6bf0a78fde0c477798d6577219fb62d821
martorres23 <patricia25tg@gmail.com>
2026-02-20 17:17:40 -0500

    cambia contactos y reestructura pagina

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// Pequeñas animaciones o mejoras futuras
 document.addEventListener("DOMContentLoaded", () => {
+  const reveals = document.querySelectorAll(".reveal");
+
+  const revealOnScroll = () => {
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = reveals[i].getBoundingClientRect().top;
+      const elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    }
+  };
+
+  window.addEventListener("scroll", revealOnScroll);
+  revealOnScroll();
+
   console.log("Sitio cargado correctamente");
 });
